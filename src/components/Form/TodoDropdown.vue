c1e7d902bc6c63c4bd20bdf7a157288d7ab1d6ea
kgurungsein <kgurung@sein.tech>
2023-12-23 21:44:42 +0000

    refact(todoform): added reusable component for form input

--- src/components/Form/TodoDropdown.vue
+++ src/components/Form/TodoDropdown.vue
@@ -1,47 +1,35 @@
 <template>
   <div>
     <label
-      :for="id"
+      for="status"
       class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+      >Status</label
     >
-      {{ label }}
-    </label>
-    <input
-      :id="id"
+    <select
       :value="modelValue"
-      @input="updateValue"
+      @input="updateModelValue"
       class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-primary-600 focus:ring-primary-600 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500"
-      :placeholder="placeholder"
-      :required="required"
-    />
+    >
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.text }}
+      </option>
+    </select>
   </div>
 </template>
 
 <script setup>
 defineProps({
-  label: {
-    type: String,
-    required: true
-  },
-  id: {
-    type: String,
-    required: true
-  },
-  placeholder: {
-    type: String
-  },
-  required: {
-    type: Boolean
-  },
-  modelValue: {
-    type: String,
-    default: ''
-  }
+  modelValue: String,
+  options: Array
 })
 
 const emit = defineEmits(['update:modelValue'])
 
-const updateValue = (event) => {
+const updateModelValue = (event) => {
   emit('update:modelValue', event.target.value)
 }
 </script>
