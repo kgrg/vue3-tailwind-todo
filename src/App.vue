d3c0e0eadce43b3faf6718ec604411a003c70d75
kgurungsein <kgurung@sein.tech>
2025-04-19 00:19:37 +0100

    feat(design): responsiveness fix

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import AppSidebar from '@/layouts/AppSidebar.vue'
 </script>
 
 <template>
-  <div class="h-screen flex">
+  <div class="min-h-screen flex flex-col lg:flex-row">
     <AppSidebar />
-    <main class="flex-1 overflow-y-auto bg-white">
+    <main class="flex-1 overflow-y-auto bg-white lg:h-screen">
       <router-view></router-view>
     </main>
   </div>
